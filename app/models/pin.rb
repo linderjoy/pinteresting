4b62d6d5a99c7cc8ecce2c62537d4ce80931740a
Linda Joy <linderjoy@gmail.com>
2016-07-15 16:49:58 -0400

    allow gifs

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,6 +3,6 @@ class Pin < ApplicationRecord
 
 	has_attached_file :image, :styles => { :large => "800x800", :medium => "300x300>", :thumb => "100x100>" }
 	validates :image, presence: true
-	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 	validates :description, presence: true
 end
