cf5cb35a5a10130eb72310a2608554a1a40141dc
Linda Joy <linderjoy@gmail.com>
2016-07-15 16:34:24 -0400

    added name to model and to pins

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,5 +2,7 @@ class Pin < ApplicationRecord
 	belongs_to :user
 
 	has_attached_file :image, :styles => { :large => "800x800", :medium => "300x300>", :thumb => "100x100>" }
+	validates :image, presence: true
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+	validates :description, presence: true
 end
